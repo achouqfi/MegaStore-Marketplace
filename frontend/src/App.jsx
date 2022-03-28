dc416da27f34b298664a36d12656ead301394f29
chantaf <badr.chantaf@gmail.com>
2022-03-28 15:35:06 +0000

    vendeur-front-end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import ClientRoutes from './Routes/Clients/routes'
+import VendeurRoutes from './Routes/Vendeurs/routes'
 function App() {
 
   return (
     <div className="App">
       <ClientRoutes />
+      <VendeurRoutes />
     </div>
   )
+
+
 }
 
 export default App
