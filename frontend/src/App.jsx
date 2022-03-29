289ee1ed2aa8e46e312cc01603a184177a2cba91
MohamedBoumlik <mohamed.boumlik.19@gmail.com>
2022-03-29 09:31:50 +0000

    admin sideBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import AdminRoutes from './Routes/Admin/Admin.routes'
 import ClientRoutes from './Routes/Clients/routes'
 function App() {
 
   return (
     <div className="App">
       <ClientRoutes />
+      <AdminRoutes/>
     </div>
   )
 }
